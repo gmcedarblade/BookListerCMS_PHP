2f0dd1b3672c3f946a6fe75c2fe69172e758edf7
Greg Cedarblade <gmcedarblade@gmail.com>
2017-05-04 16:50:37 -0500

    added delete authors page

--- deleteAuthor.php
+++ deleteAuthor.php
@@ -16,8 +16,49 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+    <?php
+    if ($_GET['authId']) {
+
+    $authorId = $_GET['authId'];
+    $authorName = $_GET['authName'];
+
+    if (isset($_POST['submit'])) {
+
+        require 'dbConnect.php';
+
+        try {
+
+            $sql = "DELETE from bookstuff WHERE authorId = :authId";
+            $sql2 = "DELETE from authors WHERE id = :authId";
+
+            $bookStatement = $pdo->prepare($sql);
+            $authorStatement = $pdo->prepare($sql2);
+
+            $bookStatement->bindValue(":authId", $_POST['authId']);
+            $authorStatement->bindValue(":authId", $_POST['authId']);
+
+            $bookStatement->execute();
+            $authorStatement->execute();
+
+            header("Location: http://localhost:9090/BookListerCMS_gcedarblade/authorsAdmin.php");
+
+        } catch (Exception $exception) {
+
+        // TODO error handling
+
+
+        }
+
+    }
+
+    }
+    ?>
+    <form action="" method="post">
+        <p>Are you sure you want to delete the author <?= $authorName ?> AND all associated books!?</p>
+        <input type="hidden" name="authId" value="<?= $authorId ?>">
+        <input type="hidden" name="authName" value="<?= $authorName ?>">
+        <a href="authorsAdmin.php"><input type="button" value="No"></a>
+        <input type="submit" name="submit" value="Yes - DO IT NOW!!">
+    </form>
     </body>
 </html>
